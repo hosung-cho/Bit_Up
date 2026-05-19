ba8914c37882da288c837dafd0ffd487b3b8e9c6
JunYeong <doct0103@naver.com>
2026-05-19 17:47:02 +0900

    심 추가

---
+++ B_8bit/src/tb/sim/run_notebook.bat
@@ -0,0 +1,16 @@
+@echo off
+REM Vivado setup path may need local adjustment.
+call F:\Xilinx\Vivado\2024.2\settings64.bat
+
+echo Vivado Tcl Batch mode simulation starts...
+call vivado -mode batch -source run_sim.tcl -notrace
+
+echo.
+echo =========================================
+echo [Cleanup] Remove unnecessary Vivado temp files
+echo =========================================
+if exist .Xil rmdir /S /Q .Xil
+del /Q vivado*.jou vivado*.log >nul 2>&1
+
+echo [Cleanup] done
+@REM pause
